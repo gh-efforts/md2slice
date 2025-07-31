eca32e533519614ab0cde43b0150720d7756d440
xutianyi <xutianyi1999@live.com>
2025-07-31 14:32:34 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
-use glob::glob;
-use tokenizers::{tokenizer, EncodeInput, Tokenizer};
 use anyhow::{anyhow, Result};
+use glob::glob;
+use tokenizers::{EncodeInput, Tokenizer};
 
 fn exec(dst: &str) -> Result<()> {
     let tokenizer = Tokenizer::from_pretrained("Qwen/Qwen3-4B", None).map_err(|e| anyhow!(e))?;
 
     let mut text_list: Vec<EncodeInput> = Vec::new();
+    let mut total_tokens = 0;
 
     for entry in glob(&format!("{}/**/*.md", dst)).unwrap().filter_map(Result::ok) {
         let r = std::fs::read_to_string(entry)?;
         text_list.push(r.into());
+
+        if text_list.len() == 200 {
+            let out = tokenizer.encode_batch_fast(std::mem::take(&mut text_list), false).map_err(|e| anyhow!(e))?;
+
+            for x in out {
+                total_tokens += x.get_ids().len();
+            }
+        }
     }
 
-    let out = tokenizer.encode_batch_fast(text_list, false)?;
-    let mut total_tokens = 0;
-
-    for x in out {
-        total_tokens += x.get_ids().len();
-    }
     println!("Total tokens: {}", total_tokens);
     Ok(())
 }
